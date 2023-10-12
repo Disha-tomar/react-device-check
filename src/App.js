a3c4b11c52312e97db9e486d6c5292aa10bb9708
Disha <dishatomariitkgp@gmail.com>
2023-10-12 20:06:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  const checkDevice = () => {
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      setIsDesktop(false);
+    } else {
+      setIsDesktop(true);
+    }
+  };
+
+  useEffect(() => {
+    checkDevice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isDesktop ? (
+        <p>You're on laptop or desktop 💻</p>
+      ) : (
+        <p>You're on mobile or tablet</p>
+      )}
     </div>
   );
 }
